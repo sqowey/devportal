f195d2fed99cc9b39505a55ef2163c4e34ff0fc7
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-20 21:50:03 +0200

    add auto redirectors

--- portal/index.php
+++ portal/index.php
@@ -1,3 +1,15 @@
+<?php 
+	// Start the session, to get the data
+	session_start();
+	// If the user is logged in redirect to the app page
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        exit;
+    } else {
+        header('Location: ../login/');
+        exit;
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 
