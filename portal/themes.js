60452e3cbd2c7737eda54a55d07b49a098f2c8ce
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-20 15:03:07 +0200

    add navigation bar

--- portal/themes.js
+++ portal/themes.js
@@ -4,6 +4,8 @@
 // Hier kannst du die Farbcodes umstellen
 ThemeColorBackLight = "#f1f1f1";
 ThemeColorBackDark = "#282C36";
+ThemeColorNavBackDark = "#384250";
+ThemeColorNavBackLight = "#d1d1d1";
 ThemeColorTextLight = "#282C36";
 ThemeColorTextDark = "#818181";
 ThemeButtonNameLight = "Hell";
@@ -12,9 +14,10 @@ ThemeButtonNameDark = "Dunkel";
 
 
 // Funktion, die die Farbänderungen auführt
-function changeToTheme(backgroundColor, textColor, themeName) {
+function changeToTheme(backgroundColor, textColor, navBackgroundColor, themeName) {
     document.body.style.backgroundColor = backgroundColor;
     document.body.style.color = textColor;
+    document.getElementById("navbar").style.backgroundColor = navBackgroundColor;
     document.getElementById("themeToggleButton").innerHTML = themeName;
 }
 
@@ -30,11 +33,11 @@ function initializeTheme() {
     if (theme == "light") {
 
         // Theme ändern
-        changeToTheme(ThemeColorBackLight, ThemeColorTextLight, ThemeButtonNameLight);
+        changeToTheme(ThemeColorBackLight, ThemeColorTextLight, ThemeColorNavBackLight, ThemeButtonNameLight);
     } else {
 
         // Theme ändern
-        changeToTheme(ThemeColorBackDark, ThemeColorTextDark, ThemeButtonNameDark);
+        changeToTheme(ThemeColorBackDark, ThemeColorTextDark, ThemeColorNavBackDark, ThemeButtonNameDark);
     }
 }
 
@@ -53,14 +56,14 @@ function toggleTheme() {
     if (theme == "dark") {
 
         // Theme ändern
-        changeToTheme(ThemeColorBackLight, ThemeColorTextLight, ThemeButtonNameLight);
+        changeToTheme(ThemeColorBackLight, ThemeColorTextLight, ThemeColorNavBackLight, ThemeButtonNameLight);
 
         // Theme-Speicher auf "Hell" setzen
         localStorage.setItem("theme", "light");
     } else {
 
         // Theme ändern
-        changeToTheme(ThemeColorBackDark, ThemeColorTextDark, ThemeButtonNameDark);
+        changeToTheme(ThemeColorBackDark, ThemeColorTextDark, ThemeColorNavBackDark, ThemeButtonNameDark);
 
         // Theme-Speicher auf "Dunkel" setzen
         localStorage.setItem("theme", "dark");
