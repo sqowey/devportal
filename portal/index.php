0fcfa21721bb38af2c3d6eb185b8546ee2d0ee6d
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-20 22:43:03 +0200

    add database getter

--- portal/index.php
+++ portal/index.php
@@ -6,6 +6,24 @@
     } else {
         header('Location: ../login/');
     }
+
+    // Variables
+    $db_config = require('../config.php');
+
+    // Conect to database
+    $con = mysqli_connect($db_host, $db_user, $db_pass, 'sqowey_devportal');
+    if (mysqli_connect_errno()) {
+        header("Location: index.html?c=98");
+    }
+
+    // Get all apps
+    $applist = array();
+    $query=$con->query("SELECT app_id, app_level, tokens, app_name, app_icon FROM apps WHERE dev_id = \"".$_SESSION['id']."\"");
+    if($query){
+        while($row = mysqli_fetch_array($query)){
+            array_push($applist, json_encode(array("app_id" => $row["app_id"], "app_level" => $row["app_level"], "tokens" => $row["tokens"], "app_name" => $row["app_name"], "app_icon" => $row["app_icon"])));
+        }
+    }
 ?>
 
 <!DOCTYPE html>
