b3a7cdf72ffc67b8815a94778f3054c0d3b22eb0
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-21 19:15:47 +0200

    remove old-artifact error messages

--- portal/message_script.js
+++ portal/message_script.js
@@ -1,15 +1,6 @@
 // The messages that refer to the codes
 code_messages_en = {
     "00": "Success",
-    "01": "This email<br>is invalid",
-    "02": "This username<br>is invalid",
-    "03": "Username length<br>must be between<br>4 and 12 characters",
-    "04": "Password is<br>invalid",
-    "11": "Username already taken",
-    "12": "Email already taken too many times",
-    "13": "Account created<br>You can login now",
-    "14": "Username or<br>password invalid",
-    "21": "Both passwords must match",
     "98": "Database error",
     "99": "ERROR"
 };
