b663952013bff8a33e0141893f66991653a9fd32
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-22 16:58:47 +0200

    remove if else nesting

--- login/login.php
+++ login/login.php
@@ -16,50 +16,47 @@
     if (mysqli_connect_errno()) {
         header("Location: index.html?c=98");
         exit;
-    } else {
+    }
 
-        // Check if account exists
-        $stmt = $con->prepare("SELECT id FROM accounts WHERE username = ?");
+    // Check if account exists
+    $stmt = $con->prepare("SELECT id FROM accounts WHERE username = ?");
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows == 0) {
+        header("Location: index.html?c=14");
+        exit;
+    }
+
+    // Get id, salt and password hash from database
+    if ($stmt = $con->prepare("SELECT id, salt, password, email, account_version FROM accounts WHERE username = ?")) {
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $stmt->store_result();
-        if ($stmt->num_rows == 0) {
-            header("Location: index.html?c=14");
-            exit;
-        } else {
-
-        // Get id, salt and password hash from database
-        if ($stmt = $con->prepare("SELECT id, salt, password, email, account_version FROM accounts WHERE username = ?")) {
-            $stmt->bind_param('s', $username);
-            $stmt->execute();
-            $stmt->store_result();
-            $stmt->bind_result($id, $salt, $password_hash, $email, $account_version);
-            $stmt->fetch();
-
-            // Add salt and password and check if right
-            if ($account_version < 2) {
-                $pw_with_hash = $password;
-            } else {
-                $pw_with_hash = $salt . $password;
-            }
-
-            // Check if password is right
-            if (!password_verify($pw_with_hash, $password_hash)) {
-                header("Location: index.html?c=14");
-                exit;
-            } else {
-
-                // Set session variables
-                $_SESSION['id'] = $id;
-                $_SESSION['username'] = $username;
-                $_SESSION['displayname'] = $displayname;
-                $_SESSION['email'] = $email;
-                $_SESSION['loggedin'] = true;
-
-                // Redirect to app
-                header("Location: ../portal/");
-            }
-            }
-        }
+        $stmt->bind_result($id, $salt, $password_hash, $email, $account_version);
+        $stmt->fetch();
     }
+
+    // Add salt and password and check if right
+    if ($account_version < 2) {
+        $pw_with_hash = $password;
+    } else {
+        $pw_with_hash = $salt . $password;
+    }
+
+    // Check if password is right
+    if (!password_verify($pw_with_hash, $password_hash)) {
+        header("Location: index.html?c=14");
+        exit;
+    } 
+
+    // Set session variables
+    $_SESSION['id'] = $id;
+    $_SESSION['username'] = $username;
+    $_SESSION['displayname'] = $displayname;
+    $_SESSION['email'] = $email;
+    $_SESSION['loggedin'] = true;
+
+    // Redirect to app
+    header("Location: ../portal/");
 ?>
