2091e1d39e776935e793e98abf4956a0a0ef061f
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-09-03 07:54:30 +0100

    Add info container

--- perms/permit/index.php
+++ perms/permit/index.php
@@ -18,11 +18,11 @@
     }
 
     // Check if app exists
-    if($stmt = $con->prepare("SELECT app_name, dev_id FROM apps WHERE app_id = ?")){
+    if($stmt = $con->prepare("SELECT app_name, dev_id, app_created FROM apps WHERE app_id = ?")){
         $stmt->bind_param("s",$_GET["app_id"]);
         $stmt->execute();
         $stmt->store_result();
-        $stmt->bind_result($app_name, $app_owner);
+        $stmt->bind_result($app_name, $app_owner, $app_creation);
         $stmt->fetch();
         $stmt->close();
         if(!isset($app_name) || !isset($app_owner)){
@@ -30,11 +30,44 @@
         }
     }
 
+    // Count number of uses for the app
+    if($stmt = $con->prepare("select * FROM permissions_users WHERE app_id = ?")){
+        $stmt->bind_param("s",$_GET["app_id"]);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_of_apps = strval($stmt->num_rows());
+        $stmt->close();
+    }
+
+
 	// If the user is logged in redirect to the app page
     if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
         header('Location: ../login/?app_id='.$_GET["app_id"]);
     }
 
+    // Open the db connection to the account db
+    $con->close();
+    $con = mysqli_connect($db_host, $db_user, $db_pass, 'accounts');
+    if (mysqli_connect_errno()) {
+        exit("DB ERROR");
+    }
+
+    // Get the app owners name
+    if($stmt = $con->prepare("SELECT displayname FROM accounts WHERE id = ?")){
+        $stmt->bind_param("s",$app_owner);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($displayname);
+        $stmt->fetch();
+        $stmt->close();
+        if(!isset($app_name) || !isset($app_owner)){
+            exit("UNKNOWN APP ID");
+        }
+    }
+
+    // Get the creation date
+    $date = date_create($app_creation);
+    $creation_formatted = date_format($date,"Y/m/d");
 ?>
 
 <!DOCTYPE html>
@@ -64,7 +97,7 @@
             <div>
                 <p>
                     The external Application<br>
-                    <span id="app_name"><?=$app_name?></span><br>
+                    <span class="emphasize"><?=$app_name?></span><br>
                     wants the following Permissions
                 </p>
             </div>
@@ -73,8 +106,27 @@
 
             </div>
             <hr>
-            <div>
-
+            <div id="info_container">
+                <table>
+                    <tr>
+                        <td><i class="fa-solid fa-code"></i></td>
+                        <td>
+                            This Application is being maintained by <span class="emphasize"><?=$displayname?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-solid fa-clock"></i></td>
+                        <td>
+                            This Application exists since <span class="emphasize"><?=$creation_formatted?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-solid fa-users"></i></td>
+                        <td>
+                        This Application is trusted by users <span class="emphasize"><?=$num_of_apps?>x Times</span>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
